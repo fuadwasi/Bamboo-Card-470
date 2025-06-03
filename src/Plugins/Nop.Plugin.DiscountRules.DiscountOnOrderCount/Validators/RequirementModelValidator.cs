ad008eac1efb0f72f56f505e43a343a6b021e50b
fuadwasi <fuad15-9400@diu.edu.bd>
2025-06-04 00:35:51 +0600

    discount plugin

---
+++ src/Plugins/Nop.Plugin.DiscountRules.DiscountOnOrderCount/Validators/RequirementModelValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Nop.Plugin.DiscountRules.DiscountOnOrderCount.Models;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Plugin.DiscountRules.DiscountOnOrderCount.Validators;
+
+/// <summary>
+/// Represents an <see cref="RequirementModel"/> validator.
+/// </summary>
+public class RequirementModelValidator : BaseNopValidator<RequirementModel>
+{
+    public RequirementModelValidator(ILocalizationService localizationService)
+    {
+        RuleFor(model => model.DiscountId)
+            .NotEmpty()
+            .WithMessageAwait(localizationService.GetResourceAsync("Plugin.DiscountRules.DiscountOnOrderCount.Fields.DiscountId.Required"));
+        RuleFor(model => model.OrderCount).GreaterThanOrEqualTo(1)
+            .WithMessageAwait(localizationService.GetResourceAsync("Plugin.DiscountRules.DiscountOnOrderCount.Fields.OrderCount.Invalid"));
+        RuleFor(model => model.OrderStatusIds)
+            .Must(statusIds => CheckOrderStatusIdshaveValue(statusIds))
+            .WithMessageAwait(localizationService.GetResourceAsync("Plugin.DiscountRules.DiscountOnOrderCount.Fields.OrderStatusIds.Invalid"));
+
+    }
+
+    private bool CheckOrderStatusIdshaveValue(IEnumerable<int> statusIds)
+    {
+        // Implement your custom condition logic here
+        return statusIds != null && statusIds.Any(); // Example condition
+    }
+}
